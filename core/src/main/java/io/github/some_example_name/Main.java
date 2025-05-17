c4a25e2e8973f64ff01c49834080b25b6189fd6f
Mohammed Labib Rafid Majumder <labib.rafid2003@gmail.com>
2025-05-18 00:15:37 +0600

    Update_Score

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
@@ -15,7 +16,7 @@ import com.badlogic.gdx.utils.TimeUtils;
 
 import java.util.Iterator;
 
-public class Main extends ApplicationAdapter{
+public class Main extends ApplicationAdapter {
     private Texture runningManSheet, backgroundTexture, obstacleTexture;
     private Batch batch;
     private Rectangle player;
@@ -32,13 +33,19 @@ public class Main extends ApplicationAdapter{
     private Animation<TextureRegion> runAnimation;
     private float stateTime = 0f;
 
+    private BitmapFont font;
+    private int score = 0;
+
     @Override
-    public void create(){
+    public void create() {
         batch = new SpriteBatch();
 
         runningManSheet = new Texture("running_man.jpg");
         backgroundTexture = new Texture("white_back.png");
         obstacleTexture = new Texture("neon_rectangle.jpg");
+
+        font = new BitmapFont(); // default font
+        font.getData().setScale(2f); // increase size
 
         int frameCols = 3;
         int frameRows = 2;
@@ -64,11 +71,11 @@ public class Main extends ApplicationAdapter{
         spawnObstacle();
     }
 
-    private void spawnObstacle(){
+    private void spawnObstacle() {
         Rectangle obstacle;
-        if(Math.random() < 0.5){
+        if (Math.random() < 0.5) {
             obstacle = new Rectangle(Gdx.graphics.getWidth(), 100, 50, 50); // ground
-        } else{
+        } else {
             obstacle = new Rectangle(Gdx.graphics.getWidth(), 180, 50, 50); // head-height
         }
         obstacles.add(obstacle);
@@ -76,21 +83,21 @@ public class Main extends ApplicationAdapter{
     }
 
     @Override
-    public void render(){
+    public void render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if(Gdx.input.isKeyPressed(Input.Keys.UP) && !isJumping && !isCrouching){
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && !isJumping && !isCrouching) {
             velocityY = 400;
             isJumping = true;
         }
 
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && !isJumping){
-            if(!isCrouching){
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && !isJumping) {
+            if (!isCrouching) {
                 isCrouching = true;
                 player.height = crouchHeight;
             }
-        }else{
-            if(isCrouching){
+        } else {
+            if (isCrouching) {
                 isCrouching = false;
                 player.height = normalHeight;
             }
@@ -99,27 +106,30 @@ public class Main extends ApplicationAdapter{
         velocityY += gravity * Gdx.graphics.getDeltaTime();
         player.y += velocityY * Gdx.graphics.getDeltaTime();
 
-        if(player.y < 100){
+        if (player.y < 100) {
             player.y = 100;
             isJumping = false;
         }
 
         backgroundX -= 100 * Gdx.graphics.getDeltaTime();
-        if(backgroundX <= -Gdx.graphics.getWidth()){
+        if (backgroundX <= -Gdx.graphics.getWidth()) {
             backgroundX = 0;
         }
 
-        if(TimeUtils.nanoTime() - lastObstacleTime > 1000000000){
+        if (TimeUtils.nanoTime() - lastObstacleTime > 1000000000) {
             spawnObstacle();
         }
 
-        for(Iterator<Rectangle> iter = obstacles.iterator(); iter.hasNext(); ){
+        for (Iterator<Rectangle> iter = obstacles.iterator(); iter.hasNext(); ) {
             Rectangle obstacle = iter.next();
             obstacle.x -= 300 * Gdx.graphics.getDeltaTime();
-            if(obstacle.x + obstacle.width < 0) iter.remove();
+            if (obstacle.x + obstacle.width < 0) {
+                iter.remove();
+                score++; // Score increases when player avoids an obstacle
+            }
 
-            if(obstacle.overlaps(player)){
-                System.out.println("Game Over!");
+            if (obstacle.overlaps(player)) {
+                System.out.println("Game Over! Final Score: " + score);
                 dispose();
                 return;
             }
@@ -135,18 +145,21 @@ public class Main extends ApplicationAdapter{
 
         batch.draw(currentFrame, player.x, player.y, player.width, player.height);
 
-        for(Rectangle obstacle : obstacles){
+        for (Rectangle obstacle : obstacles) {
             batch.draw(obstacleTexture, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
         }
+
+        font.draw(batch, "Score: " + score, 10, Gdx.graphics.getHeight() - 10); // Top-left corner
 
         batch.end();
     }
 
     @Override
-    public void dispose(){
+    public void dispose() {
         batch.dispose();
         runningManSheet.dispose();
         backgroundTexture.dispose();
         obstacleTexture.dispose();
+        font.dispose();
     }
 }
